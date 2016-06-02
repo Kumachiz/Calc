abc9a0fcf4a5512003e91c693272f386283ab587
Aaron Porter and Michael Munyaneza <kumachiz@gmail.com>
2016-06-01 19:10:05 -0700

    add subtraction and multiplication forms and form logic

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,22 @@ $(document).ready(function() {
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
   var result = add(number1, number2);
-  $('#output').text(result);
+  $('#additionOutput').text(result);
+  });
+
+  $("form#subtraction").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#subtraction1").val());
+  var number2 = parseInt($("#subtraction2").val());
+  var result = subtraction(number1, number2);
+  $('#subtractionOutput').text(result);
+  });
+
+  $("form#multiplication").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#multiplication1").val());
+  var number2 = parseInt($("#multiplication2").val());
+  var result = multiplication(number1, number2);
+  $('#multiplicationOutput').text(result);
   });
 });
-//var number1 = parseInt(prompt("Enter a number:"));
-//var number2 = parseInt(prompt("Enter another number:"));
-//var result = add(number1, number2);
-//var result = subtraction(number1, number2)
-//var result = multiplication(number1, number2);
-//var result = division(number1, number2);
-//alert(result);
